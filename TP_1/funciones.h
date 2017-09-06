14b6fa3c4f198fbc1c9077539272102b93fbcebe
Andres Florio <andres_florio@hotmail.com>
2017-09-05 21:40:37 -0300

    Update funciones.h

--- TP_1/funciones.h
+++ TP_1/funciones.h
@@ -1,10 +1,39 @@
 #ifndef FUNCIONES_H_INCLUDED
 #define FUNCIONES_H_INCLUDED
 
+/** \brief Suma dos valores enteros
+ * \param Recibe un numero entero
+ * \param Recibe un numero entero
+ * \return Devuelve un entero
+ */
 int suma(int A, int B);
+
+/** \brief Resta dos valores enteros
+ * \param Recibe un numero entero
+ * \param Recibe un numero entero
+ * \return Devuelve un entero
+ */
 int resta(int A, int B);
+
+/** \brief Divide dos valores enteros
+ * \param Recibe un numero entero
+ * \param Recibe un numero entero
+ * \return Devuelve un entero
+ */
 int division(int A, int B);
+
+/** \brief Multiplica dos valores enteros
+ * \param Recibe un numero entero
+ * \param Recibe un numero entero
+ * \return Devuelve un entero
+ */
 int multiplicar(int A, int B);
+
+/** \brief Factoriza un valor entero
+ * \param Recibe un numero entero
+ * \param
+ * \return Devuelve un entero
+ */
 int factorizar(int A);
 
 
